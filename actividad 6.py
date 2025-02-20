c5b1e1d1da8be2b99ad5bd96d1362d10c0abf63c
silver3010 <banuelos.karen.2024@cbtis-246.edu.mx>
2025-02-20 15:24:23 -0600

    Add files via upload

---
+++ actividad 6.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+tupla_1 = (1,2,3,4,5)
+print(tupla_1)
+
+
+# In[ ]:
+
+
+tupla_2 = (6,7,8,9,10)
+print(tupla_2[2])
+
+
+# In[ ]:
+
+
+tupla_1 = (1,2,3,4,5)
+tupla_2 = (6,7,8,9,10) 
+tupla_nueva = tupla_1 + tupla_2
+print (tupla_nueva)
+
+
+# In[ ]:
+
+
+tupla = ('a', 'b', 'c')
+print(tupla[0],tupla[1],tupla[2])
+
+
+# In[ ]:
+
+
+tupla = (1,2,3,4,5)
+print(2 in tupla)
+
+
+# In[ ]:
+
+
+tupla = (0,1,2,3,4,5)
+print(tupla[2:4])
+
+
+# In[ ]:
+
+
+tupla = (10,20,30,40,50)
+print (len(tupla))
+
+
+# In[ ]:
+
+
+tupla = (1,2,3,4,5)
+tupla_repetida=tupla*
+print(tupla_repetida)
+
+
+# In[ ]:
+
+
+lista = [1,2,3,]
+tupla= tuple(lista)
+print(tupla)
+
+
+# In[1]:
+
+
+tupla = (5,12,3,8,15)
+print(min(tupla))
+print(max(tupla))
+
